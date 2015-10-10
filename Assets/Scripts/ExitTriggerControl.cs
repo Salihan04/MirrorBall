ea936766d2b76d95d0f1d8caa63f994a2a2a6cc9
Salihan04 <salihan04@gmail.com>
2015-10-10 19:21:49 +0800

    Create script to print message when Ball enters, is in, or exits Exit Trigger

---
+++ Assets/Scripts/ExitTriggerControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitTriggerControl : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered trigger");
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Still in trigger");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exited trigger");
+    }
+}
