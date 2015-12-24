1208368e475d8790da49a884c3f3b4a763436e6b
Salihan04 <salihan04@gmail.com>
2015-12-24 13:29:29 +0800

    Add StartButtonController script and attach it to Start Button. Script has Click function that loads Level1 when Start Button is clicked

---
+++ Assets/Scripts/StartButtonController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButtonController : MonoBehaviour {
+
+	public void Click()
+    {
+        Application.LoadLevel("Level1");
+    }
+}
