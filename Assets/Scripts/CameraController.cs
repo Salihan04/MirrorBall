ab168344c68e1c2b8e052aef7a357b4f44d588a3
Salihan04 <salihan04@gmail.com>
2016-01-04 15:08:45 +0800

    Simulate glow effect on Ball and Ball_mirror by using point light, halo, and a new material called Glow. For now keep this effect as enabled but in future need to be disabled by default and only enable this when a certain gesture is used.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,12 +5,6 @@ public class CameraController : MonoBehaviour {
 
     public GameObject ball;
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
 	void LateUpdate () {
         transform.position = ball.transform.position;
 	}
