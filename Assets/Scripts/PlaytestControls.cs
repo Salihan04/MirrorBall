fbd126c94650683029342934f2d6d20a5b8adff5
Salihan04 <salihan04@gmail.com>
2016-02-21 13:59:42 +0800

    Write script for playtest controls to load any level and attach script to Main Camera of each scene

---
+++ Assets/Scripts/PlaytestControls.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlaytestControls : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+            SceneManager.LoadScene("Level1");
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+            SceneManager.LoadScene("Level2");
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+            SceneManager.LoadScene("Level3");
+        if (Input.GetKeyUp(KeyCode.Alpha4))
+            SceneManager.LoadScene("Level4");
+        if (Input.GetKeyUp(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
